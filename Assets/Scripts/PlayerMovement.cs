f27109cba4e735d758d227d9fff5d28f218f368e
Seth Adkins <sadkins@chapman.edu>
2020-04-11 17:12:23 -0800

    6th Video

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
-    public float runSpeed = 25f;
+    public Animator animator;
 
+    public float runSpeed = 25f;
     private float horizontalMove = 0f;
+    
     private bool jumpFlag = false;
     private bool jump = false;
 
-    public Animator animator;
-
+    public bool hasSpeedPotion = false;
+    public bool hasJumpPotion = false;
+    public float modAmount = 0f;
+    private float potionTimeMax = 10f;
+    private float potionTime = 0f;
 
     // Update is called once per frame
     void Update()
@@ -44,6 +49,19 @@ public class PlayerMovement : MonoBehaviour
     void FixedUpdate()
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+
+        if (hasJumpPotion && potionTime < potionTimeMax)
+        {
+            controller.m_JumpForceMod = modAmount;
+            potionTime += Time.fixedDeltaTime;
+        }
+        else
+        {
+            potionTime = 0f;
+            controller.m_JumpForceMod = 0f;
+            hasJumpPotion = false;
+        }
+
 
         if (jump)
         {
